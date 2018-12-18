e439133843366e2fcaa706caeb5ae15f604b5a28
Tim van Dijen <tvdijen@gmail.com>
2018-12-18 21:40:56 +0100

    Namespaces

--- www/otp.php
+++ www/otp.php
@@ -8,27 +8,27 @@
  */
 
 if (!array_key_exists('StateId', $_REQUEST)) {
-    throw new SimpleSAML_Error_BadRequest('Missing AuthState parameter.');
+    throw new \SimpleSAML\Error\BadRequest('Missing AuthState parameter.');
 }
 $authStateId = $_REQUEST['StateId'];
-$state = SimpleSAML_Auth_State::loadState($authStateId, 'yubikey:otp:init');
+$state = \SimpleSAML\Auth\State::loadState($authStateId, 'yubikey:otp:init');
 
 $error = false;
 if (array_key_exists('otp', $_POST)) { // we were given an OTP
     try {
-        if (SimpleSAML\Module\yubikey\Auth\Process\OTP::authenticate($state, $_POST['otp'])) {
-            SimpleSAML_Auth_State::saveState($state, 'yubikey:otp:init');
-            SimpleSAML_Auth_ProcessingChain::resumeProcessing($state);
+        if (\SimpleSAML\Module\yubikey\Auth\Process\OTP::authenticate($state, $_POST['otp'])) {
+            \SimpleSAML_Auth_State::saveState($state, 'yubikey:otp:init');
+            \SimpleSAML_Auth_ProcessingChain::resumeProcessing($state);
         } else {
             $error = '{yubikey:errors:invalid_yubikey}';
         }
-    } catch (InvalidArgumentException $e) {
+    } catch (\InvalidArgumentException $e) {
         $error = $e->getMessage();
     }
 }
 
-$cfg = SimpleSAML_Configuration::getInstance();
-$tpl = new SimpleSAML_XHTML_Template($cfg, 'yubikey:otp.php');
+$cfg = \SimpleSAML\Configuration::getInstance();
+$tpl = new \SimpleSAML\XHTML\Template($cfg, 'yubikey:otp.php');
 $trans = $tpl->getTranslator();
 $tpl->data['params'] = array('StateId' => $authStateId);
 $tpl->data['error'] = ($error) ? $trans->t($error) : false;
