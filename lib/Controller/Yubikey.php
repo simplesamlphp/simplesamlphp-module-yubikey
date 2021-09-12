add1111dced8c598ba93638981dc5b98f46175dd
Tim van Dijen <tim.dijen@minbzk.nl>
2021-09-12 17:08:54 +0200

    Add controllers + tests (#5)
    * Add controller + tests

---
+++ lib/Controller/Yubikey.php
@@ -0,0 +1,126 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\Module\yubikey\Controller;
+
+use InvalidArgumentException;
+use SimpleSAML\Auth;
+use SimpleSAML\Configuration;
+use SimpleSAML\Error;
+use SimpleSAML\HTTP\RunnableResponse;
+use SimpleSAML\Module\yubikey\Auth\Process\OTP;
+use SimpleSAML\Session;
+use SimpleSAML\XHTML\Template;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Controller class for the yubikey module.
+ *
+ * This class serves the different views available in the module.
+ *
+ * @package simplesamlphp/simplesamlphp-module-yubikey
+ */
+class Yubikey
+{
+    /** @var \SimpleSAML\Configuration */
+    protected Configuration $config;
+
+    /** @var \SimpleSAML\Session */
+    protected Session $session;
+
+    /**
+     * @var \SimpleSAML\Auth\State|string
+     * @psalm-var \SimpleSAML\Auth\State|class-string
+     */
+    protected $authState = Auth\State::class;
+
+    /**
+     * @var \SimpleSAML\Module\yubikey\Auth\Process\OTP|string
+     * @psalm-var \SimpleSAML\Module\yubikey\Auth\Process\OTP|class-string
+     */
+    protected $otp = OTP::class;
+
+
+    /**
+     * Controller constructor.
+     *
+     * It initializes the global configuration and session for the controllers implemented here.
+     *
+     * @param \SimpleSAML\Configuration $config The configuration to use by the controllers.
+     * @param \SimpleSAML\Session $session The session to use by the controllers.
+     *
+     * @throws \Exception
+     */
+    public function __construct(
+        Configuration $config,
+        Session $session
+    ) {
+        $this->config = $config;
+        $this->session = $session;
+    }
+
+
+    /**
+     * Inject the \SimpleSAML\Auth\State dependency.
+     *
+     * @param \SimpleSAML\Auth\State $authState
+     */
+    public function setAuthState(Auth\State $authState): void
+    {
+        $this->authState = $authState;
+    }
+
+
+    /**
+     * Inject the \SimpleSAML\Module\yubikey\Auth\Process\OTP dependency.
+     *
+     * @param \SimpleSAML\Module\yubikey\Auth\Process\OTP $otp
+     */
+    public function setOtp(OTP $otp): void
+    {
+        $this->otp = $otp;
+    }
+
+
+    /**
+     * This page asks the user to authenticate using a Yubikey.
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request The current request.
+     * @return \SimpleSAML\XHTML\Template|\SimpleSAML\HTTP\RunnableResponse
+     */
+    public function main(Request $request)
+    {
+        $stateId = $request->get('StateId');
+        if ($stateId === null) {
+            throw new Error\BadRequest('Missing AuthState parameter.');
+        }
+
+        /** @var array $state */
+        $state = $this->authState::loadState($stateId, 'yubikey:otp:init');
+
+        $error = false;
+
+        $otp = $request->get('otp');
+        if ($otp !== null) {
+            // we were given an OTP
+            try {
+                if ($this->otp::authenticate($state, $otp)) {
+                    $this->authState::saveState($state, 'yubikey:otp:init');
+                    return new RunnableResponse([Auth\ProcessingChain::class, 'resumeProcessing'], [$state]);
+                } else {
+                    $error = 'The YubiKey used is invalid. Make sure to use the YubiKey associated with your account.';
+                }
+            } catch (InvalidArgumentException $e) {
+                $error = $e->getMessage();
+            }
+        }
+
+        $t = new Template($this->config, 'yubikey:otp.twig');
+        $t->data['params'] = ['StateId' => $stateId];
+        $t->data['error'] = $error || false;
+        $t->data['autofocus'] = 'otp';
+
+        return $t;
+    }
+}
---
+++ tests/lib/Controller/YubikeyTest.php
@@ -0,0 +1,199 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\Test\Module\yubikey\Controller;
+
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+use SimpleSAML\Auth;
+use SimpleSAML\Configuration;
+use SimpleSAML\Error;
+use SimpleSAML\HTTP\RunnableResponse;
+use SimpleSAML\Module\yubikey\Controller;
+use SimpleSAML\Module\yubikey\Auth\Process\OTP;
+use SimpleSAML\Session;
+use SimpleSAML\XHTML\Template;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Set of tests for the controllers in the "yubikey" module.
+ *
+ * @covers \SimpleSAML\Module\yubikey\Controller\Yubikey
+ */
+class YubikeyTest extends TestCase
+{
+    /** @var \SimpleSAML\Configuration */
+    protected Configuration $config;
+
+    /** @var \SimpleSAML\Session */
+    protected Session $session;
+
+
+    /**
+     * Set up for each test.
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->config = Configuration::loadFromArray(
+            [
+                'module.enable' => ['yubikey' => true],
+            ],
+            '[ARRAY]',
+            'simplesaml'
+        );
+
+        $this->session = Session::getSessionFromRequest();
+
+        Configuration::setPreLoadedConfig($this->config, 'config.php');
+    }
+
+
+    /**
+     * Test that accessing the otp-endpoint without state results in an error-response
+     *
+     * @return void
+     */
+    public function testOtpNoState(): void
+    {
+        $request = Request::create(
+            '/otp',
+            'GET'
+        );
+
+        $c = new Controller\Yubikey($this->config, $this->session);
+
+        $this->expectException(Error\BadRequest::class);
+        $this->expectExceptionMessage("BADREQUEST('%REASON%' => 'Missing AuthState parameter.')");
+
+        $c->main($request);
+    }
+
+
+    /**
+     * Test that accessing the otp-endpoint without otp results in a Template
+     *
+     * @return void
+     */
+    public function testOtpNoOtp(): void
+    {
+        $request = Request::create(
+            '/otp',
+            'GET',
+            ['StateId' => 'abc123']
+        );
+
+        $c = new Controller\Yubikey($this->config, $this->session);
+        $c->setAuthState(new class () extends Auth\State {
+            public static function loadState(string $id, string $stage, bool $allowMissing = false): ?array
+            {
+                return [];
+            }
+        });
+        $response = $c->main($request);
+
+        $this->assertTrue($response->isSuccessful());
+        $this->assertInstanceOf(Template::class, $response);
+    }
+
+
+    /**
+     * Test that accessing the otp-endpoint with invalid otp returns Template
+     *
+     * @return void
+     */
+    public function testOtpFailed(): void
+    {
+        $request = Request::create(
+            '/otp',
+            'GET',
+            ['StateId' => 'abc123', 'otp' => 'aabbccddeeffgghhiijjkkllmmnnooppqq']
+        );
+
+        $c = new Controller\Yubikey($this->config, $this->session);
+        $c->setAuthState(new class () extends Auth\State {
+            public static function loadState(string $id, string $stage, bool $allowMissing = false): ?array
+            {
+                return [];
+            }
+        });
+        $c->setOtp(new class (['api_client_id' => 'phpunit', 'api_key' => 'abc123'], []) extends OTP {
+            public static function authenticate(array &$state, string $otp): bool
+            {
+                return false;
+            }
+        });
+        $response = $c->main($request);
+
+        $this->assertTrue($response->isSuccessful());
+        $this->assertInstanceOf(Template::class, $response);
+    }
+
+
+    /**
+     * Test that accessing the otp-endpoint with valid otp returns RunnableResponse
+     *
+     * @return void
+     */
+    public function testOtpSucceeded(): void
+    {
+        $request = Request::create(
+            '/otp',
+            'GET',
+            ['StateId' => 'abc123', 'otp' => 'aabbccddeeffgghhiijjkkllmmnnooppqq']
+        );
+
+        $c = new Controller\Yubikey($this->config, $this->session);
+        $c->setAuthState(new class () extends Auth\State {
+            public static function loadState(string $id, string $stage, bool $allowMissing = false): ?array
+            {
+                return [];
+            }
+        });
+        $c->setOtp(new class (['api_client_id' => 'phpunit', 'api_key' => 'abc123'], []) extends OTP {
+            public static function authenticate(array &$state, string $otp): bool
+            {
+                return true;
+            }
+        });
+        $response = $c->main($request);
+
+        $this->assertTrue($response->isSuccessful());
+        $this->assertInstanceOf(RunnableResponse::class, $response);
+    }
+
+
+    /**
+     * Test that accessing the otp-endpoint when an unexpected exception occurs returns a Template
+     *
+     * @return void
+     */
+    public function testOtpUnexpectedException(): void
+    {
+        $request = Request::create(
+            '/otp',
+            'GET',
+            ['StateId' => 'abc123', 'otp' => 'aabbccddeeffgghhiijjkkllmmnnooppqq']
+        );
+
+        $c = new Controller\Yubikey($this->config, $this->session);
+        $c->setAuthState(new class () extends Auth\State {
+            public static function loadState(string $id, string $stage, bool $allowMissing = false): ?array
+            {
+                return [];
+            }
+        });
+        $c->setOtp(new class (['api_client_id' => 'phpunit', 'api_key' => 'abc123'], []) extends OTP {
+            public static function authenticate(array &$state, string $otp): bool
+            {
+                throw new InvalidArgumentException("There was an unexpected error while trying to verify your YubiKey.");
+            }
+        });
+        $response = $c->main($request);
+
+        $this->assertTrue($response->isSuccessful());
+        $this->assertInstanceOf(Template::class, $response);
+    }
+}
